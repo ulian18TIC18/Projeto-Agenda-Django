f304bc8c0aaa8497bd8c11c39f6956a98b6a27e8
ulian18TIC18 <ulian18@gmail.com>
2024-07-17 16:35:38 -0300

    Implementado a view, a url  e o template de vizualização para um único contato

--- Projeto-Agenda-Django/contact/admin.py
+++ Projeto-Agenda-Django/contact/admin.py
@@ -6,13 +6,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = '-id', #ordem inversar do id
     list_filter = 'created_date', #filtro por data
     search_fields = 'id', 'first_name', 'last_name', #inserir busca
     list_per_page = 10 #páginação de contatos
     list_max_show_all = 200 #máximo a ser mostrado para todos os dados
-    list_editable = 'first_name', 'last_name', #preferências de edição
+    list_editable = 'first_name', 'last_name', 'show', #preferências de edição
     list_display_links = 'id', 'phone', #links de acesso ao contato específico
 
 @admin.register(models.Category)
