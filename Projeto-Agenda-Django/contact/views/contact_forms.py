a59b9fde1ebdf87b598bca821d18516bb68936e1
ulian18TIC18 <ulian18@gmail.com>
2024-07-30 15:47:25 -0300

    Incluindo mensagens de eventuais erros no formulário

--- Projeto-Agenda-Django/contact/views/contact_forms.py
+++ Projeto-Agenda-Django/contact/views/contact_forms.py
@@ -1,26 +1,15 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import render
 
-from django.core.paginator import Paginator
+from contact.forms import ContactForm
 
-from django.db.models import Q
-
-from contact.models import Contact
-
-from django import forms
 
 # Create your forms here.
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone',
-        )
 
 def create(request):
     if request.method == 'POST':
         context = {
-        'form': ContactForm(data=request.POST)        
+            'form': ContactForm(request.POST)        
         }
 
         return render(
