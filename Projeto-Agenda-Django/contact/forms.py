59dfdbc59f2c78103725676febdf2ae22d34040b
ulian18TIC18 <ulian18@gmail.com>
2024-08-16 16:55:07 -0300

    Adicionando o campo de Imagens ao formulário e alterar imagem com persistência de imagens na pasta media

--- Projeto-Agenda-Django/contact/forms.py
+++ Projeto-Agenda-Django/contact/forms.py
@@ -4,6 +4,14 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
+
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -39,41 +47,24 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = models.Contact
         fields = (
-            'first_name', 'last_name', 'phone', 'email', 'description', 'category'
+            'first_name', 'last_name', 'phone',
+            'email', 'description', 'category', 'picture'
         )
-#        widgets = {
-#            'first_name' : forms.TextInput(
-#                attrs={
-#                    'placeholder': 'Escreva aqui'
-#                }
-#            ),
-#            'last_name' : forms.TextInput(
-#                attrs={
-#                    'placeholders': 'Escreva aqui'
-#                }
-#            )
-#        }
 
     def clean(self):
-        '''cleaned_data = self.cleaned_data        
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')        
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
+        if first_name == last_name:
+            msg = ValidationError(
+                'O primeiro nome não pode ser igual ao segundo',
                 code='invalid'
             )
-        )
-
-        self.add_error(
-            'last_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+            self.add_error('fist_name', msg)
+            self.add_error('last_name', msg)
         
-        return super().clean()'''
+        return super().clean()
     
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
