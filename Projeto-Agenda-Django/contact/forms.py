4f44d63374d9f0898c70a8b135a4949d64976974
ulian18TIC18 <ulian18@gmail.com>
2024-08-19 17:16:49 -0300

    Adicionando os campos de first_name, last_name e email ao formulário User

--- Projeto-Agenda-Django/contact/forms.py
+++ Projeto-Agenda-Django/contact/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from . import models
 
@@ -82,4 +83,30 @@ class ContactForm(forms.ModelForm):
         return first_name
 
 class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email', 
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Email já existente', code='invalide')
+            )
+
+        return email
     ...
