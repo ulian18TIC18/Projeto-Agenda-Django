f88ad6f104d7b5c21015fbf2ed3c93d542ab6999
ulian18TIC18 <ulian18@gmail.com>
2024-08-21 18:24:42 -0300

    Incluindo atualizar usuário e validação de campos do formulário

--- Projeto-Agenda-Django/contact/urls.py
+++ Projeto-Agenda-Django/contact/urls.py
@@ -7,14 +7,15 @@ urlpatterns = [
     path('', views.index, name='index'),     
     path('search/', views.search, name='search'),
     
-    #criar contato CRUD
+    #CRUD Contatos
     path('contact/create/', views.create, name='create'),
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 
-    #criar Users
+    #CRUD Users
     path('user/create/', views.register, name='register'),
     path('user/login/', views.login_view, name='login'),
     path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
 ]
--- Projeto-Agenda-Django/contact/views/user_forms.py
+++ Projeto-Agenda-Django/contact/views/user_forms.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib import messages, auth
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -48,3 +48,29 @@ def logout_view(request):
     auth.logout(request)
 
     return redirect('contact:login')
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+    
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form.save()
+    return redirect('contact:user_update')
