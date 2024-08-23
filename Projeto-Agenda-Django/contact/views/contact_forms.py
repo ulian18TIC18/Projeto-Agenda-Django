b9ac1e70baccde9076c68d8b683d704fd68de6bd
ulian18TIC18 <ulian18@gmail.com>
2024-08-23 17:30:43 -0300

    Inserindo funcionalidade de criar, atualizar ou deletar contato apenas quando o User esstiver logado

--- Projeto-Agenda-Django/contact/views/contact_forms.py
+++ Projeto-Agenda-Django/contact/views/contact_forms.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
 from django.urls import reverse
-
 from contact.forms import ContactForm
-
 from contact.models import Contact
-
+from django.contrib.auth.decorators import login_required
 # Create your forms here.
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -41,6 +39,7 @@ def create(request):
         context
     )
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
@@ -76,6 +75,7 @@ def update(request, contact_id):
         context
     )
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
